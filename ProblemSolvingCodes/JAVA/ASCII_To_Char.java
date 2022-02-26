b433a1362efb1a9191b3cda512a1788c5ee5204a
Prasanth <prasanth@gmail.com>
2022-02-26 18:48:21 +0530

    changed file

--- ProblemSolvingCodes/JAVA/ASCII_To_Char.java
+++ ProblemSolvingCodes/JAVA/ASCII_To_Char.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a=97;
+        in a=97;
         char b=(char)a;
         System.out.print(b);
 	// write your code here
