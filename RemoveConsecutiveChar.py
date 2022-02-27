e7508de7f33faef71714790d9f73d5a683269a43
Prasanth <prasanth@gmail.com>
2022-02-27 19:16:43 +0530

    new commit

--- RemoveConsecutiveChar.py
+++
@@ -1,8 +0,0 @@
-a="cooooommppuuteerrr"
-b=""
-c=" "
-for i in a:
-    if(c!=i):
-        b=b+i
-        c=i
-print(b)
--- palindrome.py
+++
@@ -1,8 +0,0 @@
-a=int(input("enter the value:"))
-rev=0
-while(a>0):
-    b=a%10
-    rev=rev*10+b
-    a=a//10
-print(rev)
-
